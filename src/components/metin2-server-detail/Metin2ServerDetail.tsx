b95faa43d10e190b1455ce3fcb6c159a49befbf0
Burak Kurt <krtburak@outlook.com>
2024-07-28 19:44:58 +0300

    step 4 - detay sayfası yapısı oturtuldu

--- src/components/metin2-server-detail/Metin2ServerDetail.tsx
+++ src/components/metin2-server-detail/Metin2ServerDetail.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import teamStructureMapping from '@/mappers/teamStructureMapping'
-import Container from '../base/container'
-import data from '../../../mock-data.json'
-import CategoryStructure from '../category-structure'
+import Container from '@/components/base/container'
+import CategoryStructure from '@/components/category-structure'
+import Icon from '@/components/base/icon'
 import { IMetin2ServerDetailProps } from './types'
-import Icon from '../base/icon'
+
+import data from '../../../mock-data.json'
 
 function Metin2ServerDetail({}: IMetin2ServerDetailProps) {
   console.log(data)
@@ -15,8 +16,7 @@ function Metin2ServerDetail({}: IMetin2ServerDetailProps) {
         <div className="metin2-server-detail-in">
           <div className="server-detail-start">
             {data?.name && <h1 className="server-name">{data.name}</h1>}
-            {data?.subName && <h2 className="server-subname">{data.subName}</h2>}
-            <span className="name-hr" />
+            {data?.subName && <h2 className="server-name">{data.subName}</h2>}
             {data?.level && <span className="server-level">{data.level}</span>}
             {data?.type && <span className="server-type">{data.type}</span>}
             <div className="detail-section">
@@ -33,7 +33,7 @@ function Metin2ServerDetail({}: IMetin2ServerDetailProps) {
               </h3>
               <span className="detail-section-description multi-col">
                 <span>Oto Av</span>
-                <span className="option-y">Evet</span>
+                <span className="text-true">Evet</span>
               </span>
               <span className="detail-section-description multi-col">
                 <span>Yayıncı Sayısı</span>
@@ -45,31 +45,35 @@ function Metin2ServerDetail({}: IMetin2ServerDetailProps) {
               </span>
               <span className="detail-section-description multi-col">
                 <span>Legal Satış</span>
-                <span className="option-y">Evet</span>
+                <span className="text-true">Evet</span>
               </span>
               <span className="detail-section-description multi-col">
                 <span>Kule Farmı</span>
-                <span className="option-y">Evet</span>
+                <span className="text-true">Evet</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Dolunay & Kdp</span>
+                <span className="text-false">Düşür</span>
               </span>
               <span className="detail-section-description multi-col">
                 <span>Oto Boss</span>
-                <span className="option-n">Hayır</span>
+                <span className="text-false">Hayır</span>
               </span>
               <span className="detail-section-description multi-col">
                 <span>Battlepass</span>
-                <span className="option-y">Evet</span>
+                <span className="text-true">Evet</span>
               </span>
               <span className="detail-section-description multi-col">
                 <span>Simya</span>
-                <span className="option-n">Hayır</span>
+                <span className="text-false">Hayır</span>
               </span>
               <span className="detail-section-description multi-col">
                 <span>PVM Hasar Nesnesi</span>
-                <span className="option-y">Evet</span>
+                <span className="text-true">Evet</span>
               </span>
               <span className="detail-section-description multi-col">
                 <span>Farm Hasar Nesnesi</span>
-                <span className="option-y">Evet</span>
+                <span className="text-true">Evet</span>
               </span>
             </div>
           </div>
