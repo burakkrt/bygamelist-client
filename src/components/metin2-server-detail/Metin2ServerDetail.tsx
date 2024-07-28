12d7d83874bbb117ad5132fb247ba6d7f9db3269
Burak Kurt <krtburak@outlook.com>
2024-07-28 15:14:06 +0300

    step 1 detail

--- src/components/metin2-server-detail/Metin2ServerDetail.tsx
+++ src/components/metin2-server-detail/Metin2ServerDetail.tsx
@@ -13,7 +13,48 @@ function Metin2ServerDetail({}: IMetin2ServerDetailProps) {
       <Container size="extended">
         <div className="metin2-server-detail-in">
           <div className="server-detail-start">
-            <h1 className="server-name">{data.name}</h1>
+            {data?.name && <h1 className="server-name">{data.name}</h1>}
+            {data?.level && <span className="server-level">{data.level}</span>}
+            {data?.level && <span className="server-type">{data.type}</span>}
+            <div className="detail-section">
+              <h3 className="detail-section-title">Açılış tarihi</h3>
+              <span className="detail-section-description">28 Temmuz 2024 Cuma </span>
+            </div>
+            <div className="detail-section">
+              <h3 className="detail-section-title">Temel Özellikler</h3>
+              <span className="detail-section-description multi-col">
+                <span>Oto Av</span>
+                <span className="option-y">Evet</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Oto Boss</span>
+                <span className="option-n">Hayır</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Legal Satış</span>
+                <span className="option-y">Evet</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Drop</span>
+                <span>1 Client</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Battlepass</span>
+                <span className="option-y">Evet</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Kule Farmı</span>
+                <span className="option-y">Evet</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Simya</span>
+                <span className="option-n">Hayır</span>
+              </span>
+              <span className="detail-section-description multi-col">
+                <span>Efsunlar</span>
+                <span>PVM, Farm Hasar</span>
+              </span>
+            </div>
           </div>
           <div className="server-detail-middle">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus vel,
